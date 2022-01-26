34c927946f2d015cb18a00f319e549c4276f9cf3
mayur-505 <mayurbhuva505@gmail.com>
2022-01-26 13:04:10 +0530

    fix registration

--- app/containers/Register/RegisterPage.js
+++ app/containers/Register/RegisterPage.js
@@ -13,19 +13,18 @@ import CircleChecked from '@material-ui/icons/CheckCircleOutline';
 import CircleUnchecked from '@material-ui/icons/RadioButtonUnchecked';
 
 export default function RegisterPage() {
-  const paperStyle = { padding: '10px 10px', width: 850, margin: '20px ' };
+  const paperStyle = { padding: '20px 20px', width: 850, margin: '20px ' };
   const useStyles = makeStyles({
     typography: {
       color: '#111',
       fontSize: '27.36px',
-      fontWeight: 'bold',
-      marginTop: '144px',
       fontFamily:
         'Inter,-apple-system,BlinkMacSystemFont,"Segoe UI",Roboto,Helvetica,Arial,sans-serif,"Apple Color Emoji","Segoe UI Emoji","Segoe UI Symbol"',
     },
     typography1: {
+      marginTop: '25px',
       color: '#71717a',
-      fontSize: '17px',
+      fontSize: '19px',
 
       fontFamily:
         'Inter,-apple-system,BlinkMacSystemFont,"Segoe UI",Roboto,Helvetica,Arial,sans-serif,"Apple Color Emoji","Segoe UI Emoji","Segoe UI Symbol"',
@@ -34,18 +33,18 @@ export default function RegisterPage() {
       display: 'flex',
       marginTop: '20px',
       width: '100%',
+
       marginLeft: 'auto',
       marginRight: 'auto',
+      backgroundColor: '#f3f4f6',
     },
-
     signUpButton: {
       marginTop: '20px',
       color: 'primary',
-      fontSize: '16px',
+      fontSize: '20px',
       fontFamily:
         'Inter,-apple-system,BlinkMacSystemFont,"Segoe UI",Roboto,Helvetica,Arial,sans-serif,"Apple Color Emoji","Segoe UI Emoji","Segoe UI Symbol"',
-      borderRadius: '10px',
-      fontWeight: 'bold',
+      borderRadius: '12px',
     },
     checkbox: {
       marginTop: '20px',
@@ -54,12 +53,12 @@ export default function RegisterPage() {
       fontFamily:
         'Inter,-apple-system,BlinkMacSystemFont,"Segoe UI",Roboto,Helvetica,Arial,sans-serif,"Apple Color Emoji","Segoe UI Emoji","Segoe UI Symbol"',
       borderRadius: '10px',
-      fontWeight: 'bold',
     },
     loginButton: {
       marginTop: '20px',
+
       color: 'primary',
-      fontSize: '16px',
+      fontSize: '20px',
       fontFamily:
         'Inter,-apple-system,BlinkMacSystemFont,"Segoe UI",Roboto,Helvetica,Arial,sans-serif,"Apple Color Emoji","Segoe UI Emoji","Segoe UI Symbol"',
       marginLeft: '7px',
@@ -67,6 +66,7 @@ export default function RegisterPage() {
     },
     img: {
       backgroundColor: '#fef1f6',
+      width: '100',
     },
     root: {
       '&$focused $notchedOutline': {
@@ -82,9 +82,10 @@ export default function RegisterPage() {
     <div className={classes.main}>
       <Container maxWidth="sm">
         <Paper elevation={2} style={paperStyle}>
-          <Typography className={classes.typography}>Join</Typography>
           <Grid container spacing={2}>
             <Grid item xs={6}>
+              <Typography className={classes.typography}>Join</Typography>
+              <Typography className={classes.typography1}>Email</Typography>
               <TextField
                 className={classes.textField}
                 InputProps={{
@@ -94,8 +95,10 @@ export default function RegisterPage() {
                     notchedOutline: classes.notchedOutline,
                   },
                 }}
+                placeholder="yourstruly@example.com"
                 variant="outlined"
               />
+              <Typography className={classes.typography1}>Password</Typography>
               <TextField
                 className={classes.textField}
                 InputProps={{
@@ -109,16 +112,17 @@ export default function RegisterPage() {
                 variant="outlined"
               />
               <br />
-              <Checkbox
-                className={classes.checkbox}
-                icon={<CircleUnchecked />}
-                checkedIcon={<CircleChecked />}
-                inputProps={{ 'aria-label': 'primary checkbox' }}
-              />{' '}
-              <Typography className={classes.typography1}>
-                Sync my Youtube channel
-              </Typography>
-              <br />
+              <Grid container>
+                <Checkbox
+                  className={classes.checkbox}
+                  icon={<CircleUnchecked />}
+                  color="primary"
+                  checkedIcon={<CircleChecked />}
+                />{' '}
+                <Typography className={classes.typography1}>
+                  Sync my Youtube channel
+                </Typography>
+              </Grid>
               <Button
                 href="#"
                 variant="contained"
@@ -131,11 +135,12 @@ export default function RegisterPage() {
                 Log in
               </Button>
               <br />
-              <br />
+
               <Typography className={classes.typography1}>
                 By creating an account, you agree to our terms and confirm
-                you're over the age of 13.
+                you&apos;re over the age of 13.
               </Typography>
+              <br />
             </Grid>
             <div className={classes.img}>
               <Grid item xs={6}>
